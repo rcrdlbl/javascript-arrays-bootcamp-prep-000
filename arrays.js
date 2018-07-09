04593b8607c216a10dc04d370c13dbc7cd87f993
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-09 00:26:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,7 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = [element, ...array];
+  return array;
+}
